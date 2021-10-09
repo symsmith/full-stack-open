1b224c4ec551a88a832267e167e90ba9aba60f21
Syméon Smith <symeon.smith@gmail.com>
2021-10-09 11:42:10 +0200

    Break Content in a smaller component

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,17 +4,17 @@ const Header = (props) => (
   <h1>{props.course}</h1>
 )
 
+const Part = (props) => (
+  <p>
+    {props.part} {props.exercises}
+  </p>
+)
+
 const Content = (props) => (
   <>
-    <p>
-      {props.parts[0]} {props.exercises[0]}
-    </p>
-    <p>
-      {props.parts[1]} {props.exercises[1]}
-    </p>
-    <p>
-      {props.parts[2]} {props.exercises[2]}
-    </p>
+    <Part part={props.parts[0]} exercises={props.exercises[0]} />
+    <Part part={props.parts[1]} exercises={props.exercises[1]} />
+    <Part part={props.parts[2]} exercises={props.exercises[2]} />
   </>
 )
 
