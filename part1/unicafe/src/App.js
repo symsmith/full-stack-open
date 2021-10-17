2b0e3d6f754dc5ded7148ed587ecb665246babf9
Syméon Smith <symeon.smith@gmail.com>
2021-10-17 22:57:03 +0200

    Add statistics (Ex 1.7)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,6 +8,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -18,6 +22,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average} </p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
