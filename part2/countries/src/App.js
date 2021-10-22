ffd7c2a9c2cae6e8315b258605df1ffef96ea8e5
Syméon Smith <symeon.smith@gmail.com>
2021-10-23 01:14:10 +0200

    Add buttons and weather in the capital (Ex. 2.13 / 2.14)

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,10 +1,26 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 
-const Countries = ({ countries }) => {
-  if (countries.length === 0) return <p>No country found</p>
-  else if (countries.length === 1) {
-    const country = countries[0]
+const Country = ({ country }) => {
+  const [weather, setWeather] = useState({})
+  const [loaded, setLoaded] = useState(false)
+  useEffect(() => {
+    axios
+      .get(
+        "https://pro.openweathermap.org/data/2.5/weather?q=" +
+          country.capital[0] +
+          "&appid=" +
+          process.env.REACT_APP_WEATHER_API_KEY +
+          "&units=metric"
+      )
+      .then((result) => {
+        console.log(result.data)
+        setWeather(result.data)
+        setLoaded(true)
+      })
+  }, [country.capital])
+
+  if (loaded)
     return (
       <>
         <h1>{country.name.common}</h1>
@@ -21,13 +37,46 @@ const Countries = ({ countries }) => {
           src={country.flags.svg}
           alt={`flag of ${country.name.common}`}
         />
+        <h2>Weather in {country.capital[0]}</h2>
+        <p>
+          <strong>temperature: {Math.floor(weather.main.temp)}&deg;C</strong>
+        </p>
+        <img
+          src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+          alt="weather icon"
+        />
+        <p>
+          <strong>wind: {weather.wind.speed} m/s</strong>
+        </p>
       </>
     )
+  else return null
+}
+
+const Countries = ({ countries }) => {
+  const [countryShown, setCountryShown] = useState({})
+
+  if (countries.length === 0) return <p>No country found</p>
+  else if (countries.length === 1) {
+    const country = countries[0]
+    return <Country country={country} />
   } else if (countries.length <= 10)
     return (
       <ul>
         {countries.map((country) => (
-          <li key={country.name.common}>{country.name.common}</li>
+          <li key={country.name.common}>
+            {countryShown.name &&
+            countryShown.name.common === country.name.common ? (
+              <button onClick={() => setCountryShown({})}>hide</button>
+            ) : (
+              <button onClick={() => setCountryShown(country)}>show</button>
+            )}{" "}
+            {country.name.common}
+            {countryShown.name &&
+            countryShown.name.common === country.name.common ? (
+              <Country country={countryShown} />
+            ) : null}
+          </li>
         ))}
       </ul>
     )
