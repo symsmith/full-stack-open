565a694dd73c958845405d0947cb8f2954266850
Syméon Smith <symeon.smith@gmail.com>
2021-10-22 23:38:02 +0200

    Get initial data from server (Ex. 2.11)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
 const Filter = ({ filteringValue, handleFilteringChange }) => (
   <div>
@@ -38,15 +39,17 @@ const Persons = ({ entriesShown }) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
   const [filteringValue, setFilteringValue] = useState("")
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((result) => {
+      console.log(result.data)
+      setPersons(result.data)
+    })
+  }, [])
 
   let entriesShown = persons.filter((e) =>
     e.name.toLowerCase().includes(filteringValue.toLowerCase())
